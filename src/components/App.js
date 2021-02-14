a4f78ca6cffcc97874418d4e4dce637ba31c6871
ushajindi <57713975+ushajindi@users.noreply.github.com>
2021-02-14 16:04:19 +0300

    Первая часть готова

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,265 @@
-import React from 'react'
+import React, {useState,useEffect}  from 'react'
 import s from "./style.css"
 import {Link, Route, Switch} from "react-router-dom";
 import Home from "./Home";
 import SmallData from "./Small";
 import BigData from "./Big";
+import axios from "axios"
+
+const smallDataUrl = "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
 
 const App=(props)=>{
+    const [isLoading,setIsLoading]=useState(true)
+    const [smallData,setSmallData]=useState()
+    const [getDataStart,setGetDataStart]=useState({
+        getSmallData:false,
+        getBigData:false
+    })
+    const [bigData,setbigData]=useState()
+    useEffect(()=>{
+        if (getDataStart.getSmallData){
+            axios.get(smallDataUrl).then(data => {
+                setSmallData(data.data)
+                setIsLoading(false)
+            })
+        }
+    },[getDataStart])
+    const MapData = (data) => {
+        const [vis, setVis] = useState(false)
+        const [id, setId] = useState(null)
+
+        const getAddress = (idEl) => {
+            setId(idEl)
+            setVis(true)
+
+
+        }
+        const Address = (el, id) => {
+
+            if (el.id === id) {
+                return (
+                    <div className={s.address}>
+                        Выбран пользователь: <b>{el.firstName}</b><br/>
+                        Описание: <br/>
+                        <textarea value={el.description}/> <br/>
+                        Адрес проживания: <b>{el.address.streetAddress}</b> <br/>
+                        Город: <b>{el.address.city}</b> <br/>
+                        Провинция/штат: <b>{el.address.state}</b><br/>
+                        Индекс: <b>{el.address.zip}</b>
+                    </div>
+                )
+            }
+        }
+        return (data.map((el => {
+            return (
+                <div key={el.id}>
+                    <div onClick={() => {
+                        getAddress(el.id)
+                    }} className={s.small__inner}>
+                        <div> {el.id}</div>
+                        <div>{el.firstName}</div>
+                        <div>{el.email}</div>
+                        <div>{el.phone}</div>
+                        {vis && Address(el, id)}
+                    </div>
+
+                </div>
+
+            )
+        })))
+    }
+    const SortData = (data, key,getStateSort,setStateSort,setStateData) => {
+        if (key === "id") {
+            if (!getStateSort.sortId.sorted) {
+                setStateData(data.sort((a, b) => a.id - b.id))
+                setStateSort({
+                    sortName: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortId: {
+                        sorted:true,
+                        down:true
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+                //console.log(webData)
+
+            }
+            if (getStateSort.sortId.sorted) {
+                setStateData(data.sort((a, b) => a.id - b.id).reverse())
+                setStateSort({
+                    sortName: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+            }
+        }
+        if (key === "phone") {
+            if (!getStateSort.sortPhone.sorted) {
+                setStateData(data.sort((a, b) => a.phone[1] - b.phone[1]))
+                setStateSort({
+                    sortName: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:true,
+                        down:true
+                    },
+                })
+            }
+            if (getStateSort.sortPhone.sorted) {
+                setStateData(data.sort((a, b) => a.phone[1] - b.phone[1]).reverse())
+                setStateSort({
+                    sortName: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+            }
+        }
+        if (key === "name") {
+            if (!getStateSort.sortName.sorted) {
+                setStateData(data.sort((a,b)=>{
+                    if (a.firstName<b.firstName)return -1
+                    if (a.firstName>b.firstName)return 1
+                }))
+                setStateSort({
+                    sortName: {
+                        sorted:true,
+                        down:true
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+            }
+            if (getStateSort.sortName.sorted){
+                setStateData(data.sort((a,b)=>{
+                    if (a.firstName<b.firstName)return -1
+                    if (a.firstName>b.firstName)return 1
+                }).reverse())
+                setStateSort({
+                    sortName: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+            }
+        }
+        if (key === "email") {
+            if (!getStateSort.sortEmail.sorted) {
+                setStateData(data.sort((a,b)=>{
+                    if (a.email<b.email)return -1
+                    if (a.email>b.email)return 1
+                }))
+                setStateSort({
+                    sortName: {
+                        sorted:true,
+                        down:true
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:true,
+                        down:true
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+            }
+            if (getStateSort.sortEmail.sorted){
+                setStateData(data.sort((a,b)=>{
+                    if (a.email<b.email)return -1
+                    if (a.email>b.email)return 1
+                }).reverse())
+                setStateSort({
+                    sortName: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortId: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortEmail: {
+                        sorted:false,
+                        down:false
+                    },
+                    sortPhone: {
+                        sorted:false,
+                        down:false
+                    },
+                })
+            }
+        }
+    }
 
     return(
         <div className={s.intro}>
@@ -18,14 +272,23 @@ const App=(props)=>{
                         <Home props={props}/>
                     )
                 }}/>
-                <Route path={"/big"} component={(props)=>{
+                <Route path={"/big"} component={()=>{
                     return(
-                        <BigData props={props}/>
+                        <BigData/>
                     )
                 }}/>
-                <Route path={"/small"} component={(props)=>{
+                <Route path={"/small"} component={()=>{
                     return(
-                        <SmallData props={props}/>
+                        <SmallData smallData={{
+                            isLoading:isLoading,
+                            setIsLoading:setIsLoading,
+                            start:setGetDataStart,
+                            getStart:getDataStart.getSmallData,
+                            webData:smallData,
+                            setWebData:setSmallData,
+                            MapData,
+                            SortData
+                        }}/>
                     )
                 }}/>
             </Switch>
--- src/components/Small.js
+++ src/components/Small.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from "react"
-import axios from "axios"
 import s from "./style.css"
 
-const url = "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
+
 const json = [
     {
         id: 101,
@@ -19,8 +18,7 @@ const json = [
         description: 'et lacus magna dolor...',
     }
 ]
-const SmallData = () => {
-    const [webData, setWebData] = useState(json)
+const SmallData = ({smallData}) => {
     const [sortIcon, setSortIcon] = useState({
         sortName: {
             sorted:false,
@@ -39,286 +37,45 @@ const SmallData = () => {
             down:false
         },
     })
-    useEffect(() => {
-        axios.get(url).then(data => {
-            setWebData(data.data)
-        })
-    }, [])
-
-    const MapData = (data) => {
-        const [vis, setVis] = useState(false)
-        const [id, setId] = useState(null)
-
-        const getAddress = (idEl) => {
-            setId(idEl)
-            setVis(true)
-
-
+    useEffect(()=>{
+        if (!smallData.getStart){
+            smallData.start({getSmallData:true,})
         }
-        const Address = (el, id) => {
-
-            if (el.id === id) {
-                return (
-                    <div className={s.address}>
-                        Выбран пользователь: <b>{el.firstName}</b><br/>
-                        Описание: <br/>
-                        <textarea value={el.description}/> <br/>
-                        Адрес проживания: <b>{el.address.streetAddress}</b> <br/>
-                        Город: <b>{el.address.city}</b> <br/>
-                        Провинция/штат: <b>{el.address.state}</b><br/>
-                        Индекс: <b>{el.address.zip}</b>
-                    </div>
-                )
-            }
-        }
-        return (data.map((el => {
-            return (
-                <div key={el.id}>
-                    <div onClick={() => {
-                        getAddress(el.id)
-                    }} className={s.small__inner}>
-                        <div> {el.id}</div>
-                        <div>{el.firstName}</div>
-                        <div>{el.email}</div>
-                        <div>{el.phone}</div>
-                        {vis && Address(el, id)}
-                    </div>
-
-                </div>
-
-            )
-        })))
-    }
-    const SortData = (data, key) => {
-        if (key === "id") {
-            if (!sortIcon.sortId.sorted) {
-                setWebData(data.sort((a, b) => a.id - b.id))
-                setSortIcon({
-                    sortName: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortId: {
-                        sorted:true,
-                        down:true
-                    },
-                    sortEmail: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortPhone: {
-                        sorted:false,
-                        down:false
-                    },
-                })
-                //console.log(webData)
-
-            }
-            if (sortIcon.sortId.sorted) {
-                setWebData(data.sort((a, b) => a.id - b.id).reverse())
-                setSortIcon({
-                    sortName: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortId: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortEmail: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortPhone: {
-                        sorted:false,
-                        down:false
-                    },
-                })
-            }
-        }
-        if (key === "phone") {
-            console.log("click")
-            if (!sortIcon.sortPhone.sorted) {
-                setWebData(data.sort((a, b) => a.phone[1] - b.phone[1]))
-                setSortIcon({
-                    sortName: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortId: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortEmail: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortPhone: {
-                        sorted:true,
-                        down:true
-                    },
-                })
-            }
-            if (sortIcon.sortPhone.sorted) {
-                setWebData(data.sort((a, b) => a.phone[1] - b.phone[1]).reverse())
-                setSortIcon({
-                    sortName: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortId: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortEmail: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortPhone: {
-                        sorted:false,
-                        down:false
-                    },
-                })
-            }
-        }
-        if (key === "name") {
-            if (!sortIcon.sortName.sorted) {
-                setWebData(data.sort((a,b)=>{
-                    if (a.firstName<b.firstName)return -1
-                    if (a.firstName>b.firstName)return 1
-                }))
-                setSortIcon({
-                                    sortName: {
-                                        sorted:true,
-                                        down:true
-                                    },
-                                    sortId: {
-                                        sorted:false,
-                                        down:false
-                                    },
-                                    sortEmail: {
-                                        sorted:false,
-                                        down:false
-                                    },
-                                    sortPhone: {
-                                        sorted:false,
-                                        down:false
-                                    },
-                })
-            }
-            if (sortIcon.sortName.sorted){
-                setWebData(data.sort((a,b)=>{
-                    if (a.firstName<b.firstName)return -1
-                    if (a.firstName>b.firstName)return 1
-                }).reverse())
-                setSortIcon({
-                    sortName: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortId: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortEmail: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortPhone: {
-                        sorted:false,
-                        down:false
-                    },
-                })
-            }
-      }
-        if (key === "email") {
-            if (!sortIcon.sortEmail.sorted) {
-                //setWebData(webData.map(el=>{el.email.toLowerCase()}))
-                setWebData(data.sort((a,b)=>{
-                    if (a.email<b.email)return -1
-                    if (a.email>b.email)return 1
-                }))
-                setSortIcon({
-                                    sortName: {
-                                        sorted:true,
-                                        down:true
-                                    },
-                                    sortId: {
-                                        sorted:false,
-                                        down:false
-                                    },
-                                    sortEmail: {
-                                        sorted:true,
-                                        down:true
-                                    },
-                                    sortPhone: {
-                                        sorted:false,
-                                        down:false
-                                    },
-                })
-            }
-            if (sortIcon.sortEmail.sorted){
-                setWebData(data.sort((a,b)=>{
-                    if (a.email<b.email)return -1
-                    if (a.email>b.email)return 1
-                }).reverse())
-                setSortIcon({
-                    sortName: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortId: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortEmail: {
-                        sorted:false,
-                        down:false
-                    },
-                    sortPhone: {
-                        sorted:false,
-                        down:false
-                    },
-                })
-            }
-      }
-    }
-
-
+    })
     return (
-        <div>
-            <div className={s.small__inner}>
-                <div onClick={() => {
-                    SortData(webData, "id")
-                }}>{!sortIcon.sortId.down ? <i className="bi bi-arrow-bar-up"></i> :
-                    <i className="bi bi-arrow-bar-down"></i>} id
+        <>{smallData.isLoading ? <div className={s.loading}>
+                <div className="spinner-grow text-primary" role="status">
+                    <span className="sr-only"> <br/><br/><br/>Loading...</span>
                 </div>
-                <div onClick={() => {
-                    SortData(webData, "name")
-                }}>{!sortIcon.sortName.down ? <i className="bi bi-arrow-bar-up"></i> :
-                    <i className="bi bi-arrow-bar-down"></i>} firstName
+            </div> :<div>
+                <div className={s.small__inner}>
+                    <div onClick={() => {
+                        smallData.SortData(smallData.webData, "id", sortIcon, setSortIcon, smallData.setWebData)
+                    }}>{!sortIcon.sortId.down ? <i className="bi bi-arrow-bar-up"></i> :
+                        <i className="bi bi-arrow-bar-down"></i>} id
+                    </div>
+                    <div onClick={() => {
+                        smallData.SortData(smallData.webData, "name", sortIcon, setSortIcon, smallData.setWebData)
+                    }}>{!sortIcon.sortName.down ? <i className="bi bi-arrow-bar-up"></i> :
+                        <i className="bi bi-arrow-bar-down"></i>} firstName
+                    </div>
+                    <div onClick={() => {
+                        smallData.SortData(smallData.webData, "email", sortIcon, setSortIcon, smallData.setWebData)
+                    }}>{!sortIcon.sortEmail.down ? <i className="bi bi-arrow-bar-up"></i> :
+                        <i className="bi bi-arrow-bar-down"></i>} email
+                    </div>
+                    <div onClick={() => {
+                        smallData.SortData(smallData.webData, "phone", sortIcon, setSortIcon, smallData.setWebData)
+                    }}>{!sortIcon.sortPhone.down ? <i className="bi bi-arrow-bar-up"></i> :
+                        <i className="bi bi-arrow-bar-down"></i>} phone
+                    </div>
                 </div>
-                <div onClick={()=>{SortData(webData,"email")}}>{!sortIcon.sortEmail.down ? <i className="bi bi-arrow-bar-up"></i> :
-                    <i className="bi bi-arrow-bar-down"></i>} email
-                </div>
-                <div onClick={()=>{SortData(webData,"phone")}}>{!sortIcon.sortPhone.down ? <i className="bi bi-arrow-bar-up"></i> :
-                    <i className="bi bi-arrow-bar-down"></i>} phone
-                </div>
+                {smallData.MapData(smallData.webData)}
+
             </div>
-            {MapData(webData)}
-        </div>
+        }
+            </>
     )
 }
 export default SmallData
 
-
-/*  <div className={vis?s.address:s.hidden}>
-                    Выбран пользователь:  <b>{el.firstName}</b><br/>
-                    Описание: <br/>
-                    <textarea value={el.description}/> <br/>
-                    Адрес проживания: <b>{el.address.streetAddress}</b> <br/>
-                    Город: <b>{el.address.city}</b> <br/>
-                    Провинция/штат: <b>{el.address.state}</b><br/>
-                    Индекс: <b>{el.address.zip}</b>
-                </div>
-            )*/
